0a9552e6067b4b2ade22b023df4277c1a5cc9e44
unknown <william.z.wilson95@gmail.com>
2020-06-25 11:09:56 -0500

    'wf'

--- foundation/src/main/java/com/lambdaschool/foundation/config/DataSourceConfig.java
+++ foundation/src/main/java/com/lambdaschool/foundation/config/DataSourceConfig.java
@@ -10,19 +10,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
-
 import javax.sql.DataSource;
-
 @Configuration
 public class DataSourceConfig
 {
     private static boolean stop = false;
-
     @Autowired
     private ApplicationContext appContext;
     @Autowired
     private Environment env;
-
     private static void checkEnvironmentVariable(String envvar)
     {
         if (System.getenv(envvar) == null)
@@ -30,7 +26,6 @@ public class DataSourceConfig
             stop = true;
         }
     }
-
     @Bean(name = "dsCustom")
     public DataSource dataSource()
     {
@@ -38,23 +33,19 @@ public class DataSourceConfig
         String myDriverClass = "";
         String myDBUser = "";
         String myDBPassword = "";
-
         String dbValue = env.getProperty("local.run.db");
-
         if (dbValue.equalsIgnoreCase("POSTGRESQL"))
         {
             checkEnvironmentVariable("MYDBHOST");
             checkEnvironmentVariable("MYDBNAME");
             checkEnvironmentVariable("MYDBUSER");
             checkEnvironmentVariable("MYDBPASSWORD");
-
             if (stop)
             {
                 int exitCode = SpringApplication.exit(appContext,
-                                                      (ExitCodeGenerator) () -> 1);
+                        (ExitCodeGenerator) () -> 1);
                 System.exit(exitCode);
             }
-
             myUrlString = "jdbc:postgresql://" + System.getenv("MYDBHOST") + ":5432/" + System.getenv("MYDBNAME");
             myDriverClass = "org.postgresql.Driver";
             myDBUser = System.getenv("MYDBUSER");
@@ -67,7 +58,6 @@ public class DataSourceConfig
             myDBUser = "sa";
             myDBPassword = "";
         }
-
         return DataSourceBuilder.create()
                 .username(myDBUser)
                 .password(myDBPassword)
@@ -75,9 +65,7 @@ public class DataSourceConfig
                 .driverClassName(myDriverClass)
                 .build();
     }
-
     @Bean(name = "jdbcCustom")
-
     @Autowired
     public JdbcTemplate jdbcTemplate(
             @Qualifier("dsCustom")
